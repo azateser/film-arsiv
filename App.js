a70e08ad2981e6be0647c9d4df34e0b1966a0cfd
Azat <azatesser@gmail.com>
2024-11-07 12:37:58 +0300

    Initial commit
    Generated by create-expo-app 3.0.0.

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { useEffect } from "react"
 import { Text, View } from "react-native";
 import * as SplashScreen from "expo-splash-screen";
@@ -32,3 +33,25 @@ export default function App() {
   </>
   )
 }
+=======
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+>>>>>>> 923e113 (Initial commit)
